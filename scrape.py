0ce83113c519629b56225c6634526041fcab6b9b
Alex Erf <alexerf@umich.edu>
2018-04-27 00:12:25 -0700

    gets first game woooo

--- scrape.py
+++ scrape.py
@@ -8,10 +8,11 @@ page = urllib2.urlopen(schedule_page)
 
 soup = BeautifulSoup(page, 'html.parser')
 
-time = soup.find(id='showschedule').find(class_='tablehead').contents[2].contents[0].get_text()
+date = soup.find(id='showschedule').find(class_='tablehead').contents[2].contents[0].get_text()
 opponent = soup.find(id='showschedule').find(class_='tablehead').contents[2].find(class_='team-name').find('a').get_text()
 status = soup.find(id='showschedule').find(class_='tablehead').contents[2].find(class_='game-status').get_text()
+time = soup.find(id='showschedule').find(class_='tablehead').contents[2].contents[2].get_text()
 
-print time + ' ' + opponent + ' ' + status
+print 'Michigan' + ' ' + status + ' ' + opponent
+print time.strip() + ' | ' + date.strip()
 
-# print(soup.find(id='showschedule').find(class_='tablehead').contents[2].find(class_='game-schedule').prettify())
